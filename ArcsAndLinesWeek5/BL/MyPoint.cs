b82941a121629fe6b096920461ffb037c5223184
Abdullah03096344305 <abdullah03096344305@gmail.com>
2023-06-02 14:11:33 +0500

    Add project files.

---
+++ ArcsAndLinesWeek5/BL/MyPoint.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArcsAndLinesWeek5.BL
+{
+    public class MyPoint
+    {
+        public int x;
+        public int y;
+
+        public MyPoint()
+        {
+            x = 0;
+            y = 0;
+        }
+
+        public MyPoint(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public void SetXY(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public double DistanceWithCords(int x, int y)
+        {
+            int xDiff = this.x - x;
+            int yDiff = this.y - y;
+            return Math.Sqrt(xDiff * xDiff + yDiff * yDiff);
+        }
+
+        public double DistanceWithObject(MyPoint another)
+        {
+            int xDiff = this.x - another.x;
+            int yDiff = this.y - another.y;
+            return Math.Sqrt(xDiff * xDiff + yDiff * yDiff);
+        }
+
+        public double DistanceFromZero()
+        {
+            return Math.Sqrt(x * x + y * y);
+        }
+    }
+
+    public class MyLine
+    {
+        public MyPoint beginPoint;
+        public MyPoint endPoint;
+
+        public MyLine(int x1, int y1, int x2, int y2)
+        {
+            beginPoint = new MyPoint(x1, y1);
+            endPoint = new MyPoint(x2, y2);
+        }
+
+        public double Length()
+        {
+            return beginPoint.DistanceWithObject(endPoint);
+        }
+
+        public double Gradient()
+        {
+            int xDiff = endPoint.x - beginPoint.x;
+            int yDiff = endPoint.y - beginPoint.y;
+            return (double)yDiff / xDiff;
+        }
+    }
+}
---
+++ ArcsAndLinesWeek5/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ArcsAndLinesWeek5.BL;
+using System.Threading.Tasks;
+
+namespace ArcsAndLinesWeek5
+{
+    
+
+    public class Program
+    {
+        public static void Main()
+        {
+            MyLine line = null;
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("1. Make a Line");
+                Console.WriteLine("2. Update the begin point");
+                Console.WriteLine("3. Update the end point");
+                Console.WriteLine("4. Show the begin Point");
+                Console.WriteLine("5. Show the end point");
+                Console.WriteLine("6. Get the Length of the line");
+                Console.WriteLine("7. Get the Gradient of the Line");
+                Console.WriteLine("8. Find the distance of begin point from zero coordinates");
+                Console.WriteLine("9. Find the distance of end point from zero coordinates");
+                Console.WriteLine("10. Exit");
+                Console.WriteLine();
+                Console.Write("Enter your choice: ");
+                int choice = int.Parse(Console.ReadLine());
+                if (choice == 1)
+                {
+                    Console.Clear();
+                    Console.Write("Enter the x-coordinate of the begin point: ");
+                    int x1 = int.Parse(Console.ReadLine());
+                    Console.Write("Enter the y-coordinate of the begin point: ");
+                    int y1 = int.Parse(Console.ReadLine());
+                    Console.Write("Enter the x-coordinate of the end point: ");
+                    int x2 = int.Parse(Console.ReadLine());
+                    Console.Write("Enter the y-coordinate of the end point: ");
+                    int y2 = int.Parse(Console.ReadLine());
+                    line = new MyLine(x1, y1, x2, y2);
+                    Console.WriteLine("Line created.");
+                }
+                else if (choice == 2)
+                {
+                    Console.Clear();
+                    Console.Write("Enter the new x-coordinate of the begin point: ");
+                    int x = int.Parse(Console.ReadLine());
+                    Console.Write("Enter the new y-coordinate of the begin point: ");
+                    int y = int.Parse(Console.ReadLine());
+                    line.beginPoint.SetXY(x, y);
+                    Console.WriteLine("Begin point updated.");
+                }
+                else if (choice == 3)
+                {
+                    Console.Clear();
+                    Console.Write("Enter the new x-coordinate of the end point: ");
+                    int x = int.Parse(Console.ReadLine());
+                    Console.Write("Enter the new y-coordinate of the end point: ");
+                    int y = int.Parse(Console.ReadLine());
+                    line.endPoint.SetXY(x, y);
+                    Console.WriteLine("End point updated.");
+                }
+                else if (choice == 4)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Begin Point: (" + line.beginPoint.x + ", " + line.beginPoint.y + ")");
+                }
+                else if (choice == 5)
+                {
+                    Console.Clear();
+                    Console.WriteLine("End Point: (" + line.endPoint.x + ", " + line.endPoint.y + ")");
+                }
+                else if (choice == 6)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Length of the line: " + line.Length());
+                }
+                else if (choice == 7)
+                {
+                    Console.WriteLine("Gradient of the line: " + line.Gradient());
+                }
+                else if (choice == 8)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Distance of the begin point from zero coordinates: " + line.beginPoint.DistanceFromZero());
+                }
+                else if (choice == 9)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Distance of the end point from zero coordinates: " + line.endPoint.DistanceFromZero());
+                }
+                else if (choice == 10)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Exiting...");
+                    break;
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Invalid choice. Please enter a valid option.");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
